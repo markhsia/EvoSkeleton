c6a62a3d3dc14e36cda7ccf53493950ce4e040bc
CubicPower <55009831+markhsia@users.noreply.github.com>
2022-03-27 16:21:54 +0800

    Update loss.py

--- libs/hhr/core/loss.py
+++ libs/hhr/core/loss.py
@@ -104,10 +104,10 @@ def get_max_preds_soft_pt(batch_heatmaps):
     x = batch_heatmaps.sum(dim = 2)
     y = batch_heatmaps.sum(dim = 3)
     #x_indices = torch.cuda.comm.broadcast(torch.arange(width).type(torch.cuda.FloatTensor), devices=[x.device.index])[0]
-    x_indices = torch.arange(width).type(torch.cuda.FloatTensor).to([x.device.index])[0])
+    x_indices = torch.arange(width).type(torch.cuda.FloatTensor).to([x.device.index][0])
     x_indices = x_indices.view(1,1,width)
     #y_indices = torch.cuda.comm.broadcast(torch.arange(height).type(torch.cuda.FloatTensor), devices=[x.device.index])[0]
-    y_indices = torch.arange(height).type(torch.cuda.FloatTensor).to([x.device.index])[0])
+    y_indices = torch.arange(height).type(torch.cuda.FloatTensor).to([x.device.index][0])
     y_indices = y_indices.view(1,1,height)    
     x *= x_indices
     y *= y_indices
